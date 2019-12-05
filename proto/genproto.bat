5b8228ff4e5d50c188bd9e9ae59581c830340808
Edward <crazybber@outlook.com>
2019-12-05 12:31:25 +0800

    Update genproto.bat

--- proto/genproto.bat
+++ proto/genproto.bat
@@ -1,9 +1,35 @@
-@echo off
+@rem Copyright 2019 ess authors.
+@rem Generate the Go code for .proto files
+@rem choco install -y protoc
+@rem go get -d -u github.com/micro/protoc-gen-micro
+@rem go get -d -u github.com/golang/protobuf/protoc-gen-go
+@rem visit https://github.com/micro/protoc-gen-micro for this tools
 
-cd %cd%
 
-echo %cd%
+@echo off &TITLE Generation Protobuf Code For Go
 
-protoc  --micro_out=. --go_out=. message.proto
+mode con cols=100 lines=30
+color 0D
+cls
 
-pause
+setlocal
+
+
+@rem enter this directory of bat
+cd /d %~dp0
+
+echo.
+echo ## Current Dir: %cd%
+echo.
+
+@rem compile *.proto to go code
+
+protoc -I. --micro_out=. --go_out=. message.proto
+
+echo.
+echo..........work had been done.
+echo.
+echo..........code had been generated to :  %cd%
+echo.
+echo..........press any key to exit
+pause >nul
